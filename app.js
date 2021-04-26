71e16da8fe63f506bb595d27a1512bceeb83f1f7
Ibrahim-Khdairat <ibrahimkuderat@gmail.com>
2021-04-25 21:59:55 -0700

    lab01b

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 alert ('Welcome To Our Web Site');
 let userName=prompt('Please Enter your Name ');
+
 document.write('Welcome '+userName);
+ console.log(userName);
 document.write('<br>');
 let age=prompt('Please Enter Your Age ');
 document.write('Your Age is '+age);
+console.log(age);
 document.write('<br>');
 let ans=prompt('Is That your first visit To Our WebSite ?');
 if (ans=='yes')
